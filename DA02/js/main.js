c966045f4a222425b43c79f320e843e47d59a547
Camden-Levinson <clevinso@masonlive.gmu.edu>
2020-02-19 13:37:06 -0500

    DA02

--- DA02/js/main.js
+++ DA02/js/main.js
@@ -11,7 +11,9 @@ var config = {
 };
 
 var group;
-
+var x;
+var y;
+var move = 0;
 
 var game = new Phaser.Game(config);
 
@@ -22,9 +24,15 @@ function preload ()
 
 function create ()
 {
-    this.add.aprite(400, 300, 'ghost');
+    this.add.sprite(400, 300, 'ghost');
 
     group = this.add.group({key: 'ghost', frameQuantity: 1});
+    /*this.input.on('pointermove', function (pointer) {
+
+        x = pointer.x;
+        y = pointer.y;
+
+    });*/
 
     //  Display the game stats
     //info = this.add.text(10, 10, '', { font: '48px Arial', fill: '#FFFFFF' });
@@ -35,11 +43,18 @@ function create ()
 function update ()
 {
     //info.setText('Points: ' + alive + '\nTime: ' + Math.floor(10000 - timer.getElapsed()));
-    while(true){
+    /*while(true){
         Phaser.Actions.ShiftPosition(group.getChildren(), 100, 100);
         Phaser.Actions.ShiftPosition(group.getChildren(), 700, 100);
         Phaser.Actions.ShiftPosition(group.getChildren(), 700, 500);
         Phaser.Actions.ShiftPosition(group.getChildren(), 100, 500);
-    }
+    }*/
+    /*move += delta;
+
+    if (move > 6)
+    {
+        Phaser.Actions.ShiftPosition(group.getChildren(), x, y);
+        move = 0;
+    }*/
 }
 

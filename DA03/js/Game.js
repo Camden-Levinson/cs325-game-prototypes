3b37a1b6252ac873c9bd2eb8be82410088ac0c3c
Camden-Levinson <clevinso@masonlive.gmu.edu>
2020-02-26 19:42:59 -0500

    DA03

--- DA03/js/Game.js
+++ DA03/js/Game.js
@@ -18,138 +18,119 @@ GameStates.makeGame = function( game, shared ) {
     
         var GameScene = new Phaser.Class({
 
-        Extends: Phaser.Scene,
+            Extends: Phaser.Scene,
 
-        initialize:
+            initialize:
 
-        function GameScene ()
-        {
-            Phaser.Scene.call(game, { key: 'gameScene', active: true });
-
-            game.player = null;
-            game.cursors = null;
-            game.score = 0;
-            game.scoreText = null;
-        },
-
-        create: function ()
-        {
-            game.add.image(400, 300, 'sky');
-
-            var platforms = game.physics.add.staticGroup();
-
-            platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-            platforms.create(600, 400, 'ground');
-            platforms.create(50, 250, 'ground');
-            platforms.create(750, 220, 'ground');
-
-            var player = game.physics.add.sprite(100, 450, 'dude');
-
-            player.setBounce(0.2);
-            player.setCollideWorldBounds(true);
-
-            game.anims.create({
-                key: 'left',
-                frames: game.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-                frameRate: 10,
-                repeat: -1
-            });
-
-            game.anims.create({
-                key: 'turn',
-                frames: [ { key: 'dude', frame: 4 } ],
-                frameRate: 20
-            });
-
-            game.anims.create({
-                key: 'right',
-                frames: game.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-                frameRate: 10,
-                repeat: -1
-            });
-
-            game.cursors = game.input.keyboard.createCursorKeys();
-
-            var stars = game.physics.add.group({
-                key: 'star',
-                repeat: 11,
-                setXY: { x: 12, y: 0, stepX: 70 }
-            });
-
-            stars.children.iterate(function (child) {
-
-                child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-            });
-
-            game.scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-            game.physics.add.collider(player, platforms);
-            game.physics.add.collider(stars, platforms);
-
-            game.physics.add.overlap(player, stars, game.collectStar, null, game);
-
-            game.player = player;
-        },
-
-        update: function ()
-        {
-            var cursors = game.cursors;
-            var player = game.player;
-
-            if (cursors.left.isDown)
+            function GameScene ()
             {
-                player.setVelocityX(-160);
+                Phaser.Scene.call(game, { key: 'gameScene', active: true });
 
-                player.anims.play('left', true);
-            }
-            else if (cursors.right.isDown)
+                game.player = null;
+                game.cursors = null;
+                game.score = 0;
+                game.scoreText = null;
+            },
+
+            create: function ()
             {
-                player.setVelocityX(160);
+                game.add.image(400, 300, 'sky');
 
-                player.anims.play('right', true);
-            }
-            else
+                var platforms = game.physics.add.staticGroup();
+
+                platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+                platforms.create(600, 400, 'ground');
+                platforms.create(50, 250, 'ground');
+                platforms.create(750, 220, 'ground');
+
+                var player = game.physics.add.sprite(100, 450, 'dude');
+
+                player.setBounce(0.2);
+                player.setCollideWorldBounds(true);
+
+                game.anims.create({
+                    key: 'left',
+                    frames: game.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+                    frameRate: 10,
+                    repeat: -1
+                });
+
+                game.anims.create({
+                    key: 'turn',
+                    frames: [ { key: 'dude', frame: 4 } ],
+                    frameRate: 20
+                });
+
+                game.anims.create({
+                    key: 'right',
+                    frames: game.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+                    frameRate: 10,
+                    repeat: -1
+                });
+
+                game.cursors = game.input.keyboard.createCursorKeys();
+
+                var stars = game.physics.add.group({
+                    key: 'star',
+                    repeat: 11,
+                    setXY: { x: 12, y: 0, stepX: 70 }
+                });
+
+                stars.children.iterate(function (child) {
+
+                    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+                });
+
+                game.scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+                game.physics.add.collider(player, platforms);
+                game.physics.add.collider(stars, platforms);
+
+                game.physics.add.overlap(player, stars, game.collectStar, null, game);
+
+                game.player = player;
+            },
+
+            update: function ()
             {
-                player.setVelocityX(0);
+                var cursors = game.cursors;
+                var player = game.player;
 
-                player.anims.play('turn');
-            }
+                if (cursors.left.isDown)
+                {
+                    player.setVelocityX(-160);
 
-            if (cursors.up.isDown && player.body.touching.down)
+                    player.anims.play('left', true);
+                }
+                else if (cursors.right.isDown)
+                {
+                    player.setVelocityX(160);
+
+                    player.anims.play('right', true);
+                }
+                else
+                {
+                    player.setVelocityX(0);
+
+                    player.anims.play('turn');
+                }
+
+                if (cursors.up.isDown && player.body.touching.down)
+                {
+                    player.setVelocityY(-330);
+                }
+            },
+
+            collectStar: function (player, star)
             {
-                player.setVelocityY(-330);
+                star.disableBody(true, true);
+
+                game.score += 10;
+                game.scoreText.setText('Score: ' + game.score);
             }
-        },
 
-        collectStar: function (player, star)
-        {
-            star.disableBody(true, true);
-
-            game.score += 10;
-            game.scoreText.setText('Score: ' + game.score);
-        }
-
-    });
-
-    var config = {
-        type: Phaser.AUTO,
-        scale: {
-            mode: Phaser.Scale.FIT,
-            parent: 'phaser-example',
-            autoCenter: Phaser.Scale.CENTER_BOTH,
-            width: 800,
-            height: 600
-        },
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: { y: 300 },
-                debug: false
-            }
-        },
-        scene: GameScene
-    };
+        });
 
 };

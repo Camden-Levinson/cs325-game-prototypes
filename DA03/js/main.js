5f80de4d9b7581ba0c6c99f1640f09d03ed39798
Camden-Levinson <clevinso@masonlive.gmu.edu>
2020-02-26 21:47:24 -0500

    DA03

--- DA03/js/main.js
+++ DA03/js/main.js
@@ -1,10 +1,30 @@
 "use strict";
 
 window.onload = function() {
+	var GameScene = new Phaser.Scene('game');
+	var config = {
+        type: Phaser.AUTO,
+        scale: {
+            mode: Phaser.Scale,
+            parent: 'game',
+            autoCenter: Phaser.Scale.CENTER_BOTH,
+            width: 800,
+            height: 600
+        },
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false
+            }
+        },
+        scene: GameScene
+    };
+
 
 	//	Create your Phaser game and inject it into the 'game' div.
 	//	We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
-	var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
+	var game = new Phaser.Game(config);
 
 	//	Add the States your game has.
 	//	You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
@@ -13,7 +33,8 @@ window.onload = function() {
 	// the high score if you want.
 	var shared = {};
 
-	game.state.add( 'Boot', GameStates.makeBoot( game ) );
+	//game.state.add( 'Boot', GameStates.makeBoot( game ) );
+	Phaser.Scene.call(game, {key:'GameScene1'})
 	game.state.add( 'Preloader', GameStates.makePreloader( game ) );
 	game.state.add( 'MainMenu', GameStates.makeMainMenu( game, shared ) );
 	game.state.add( 'Game', GameStates.makeGame( game, shared ) );
